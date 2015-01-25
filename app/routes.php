6ad97275f6a7072ec318e8b9a4f3335a47cd6e6c
Tim Clark <tim@agylelabs.com>
2015-01-25 19:29:58 +1100

    Created all of the migrations and models, most of the controllers and some views.

--- app/routes.php
+++ app/routes.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', 'LoginController@showLogin');
 
 Route::get('/login', 'LoginController@showLogin');
 Route::post('/login', 'LoginController@login');
