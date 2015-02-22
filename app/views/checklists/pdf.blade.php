3a271fdd4caecf500783513b25d78ca8fd593197
Tim Clark <tim@agylelabs.com>
2015-02-22 15:17:48 +1100

    Fixed styles and photos on PDF's.

--- app/views/checklists/pdf.blade.php
+++ app/views/checklists/pdf.blade.php
@@ -16,59 +16,59 @@
 
 @section('content')
     <div class="content">
-        <h1>Job Number {{ $checklist->job_number or 'N/A'}}</h1>
-        <div class="row">
-            <div class="col-md-4">
-                <p><strong>Client:</strong> {{ $checklist->client->name }}</p>
-            </div>
-            <div class="col-md-8">
-                <p><strong>Address:</strong> {{ $checklist->address }}</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">
-                <p><strong>Weather:</strong> {{ $checklist->weather }}</p>
-            </div>
-            <div class="col-md-4">
-                <p><strong>Conducted at:</strong> {{ $checklist->conducted_at->toDayDateTimeString() }}</p>
-            </div>
-        </div>
-        @foreach($checklist->cl_sections as $section)
+        <div class="page-content white-bg" style="margin-left:0;">
+            <h1>Job Number {{ $checklist->job_number or 'N/A'}}</h1>
             <div class="row">
-                <div class="col-md-12">
-                    <h2>{{ $section->cl_section_template->name }} {{ $section->cl_section_template->subsection_titles }}</h2>
-                    @foreach($section->cl_subsections as $subsection)
-                        @if(count($subsection->cl_questions) > 0 || strlen($subsection->comments) > 0)
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h4>{{ $subsection->cl_subsection_template->name }}</h4>
-                                    @if(strlen($subsection->comments) > 0)
-                                        <p><strong>Notes:</strong> {{ $subsection->comments or 'none' }}</p>
-                                    @endif
-                                    @foreach($subsection->cl_questions as $question)
-                                        @if(!$question->pass)
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <p><strong>{{ $i++ }}. {{ $question->cl_question_template->question }} : </strong>{{ $question->answer }}</p>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    @foreach($question->question_images as $image)
-                                                        <div class="col-md-6">
-                                                            <img src="{{ Request::root() }}/photos/{{ $image->filename }}" style="max-width:100%" />
-                                                        </div>
-                                                    @endforeach
-                                                </div>
-                                            </div>
-                                        @endif
-                                    @endforeach
-                                </div>
-                            </div>
-                        @endif
-                    @endforeach
+                <div class="col-md-4">
+                    <p><strong>Client:</strong> {{ $checklist->client->name }}</p>
+                </div>
+                <div class="col-md-8">
+                    <p><strong>Address:</strong> {{ $checklist->address }}</p>
                 </div>
             </div>
-        @endforeach
+            <div class="row">
+                <div class="col-md-4">
+                    <p><strong>Weather:</strong> {{ $checklist->weather }}</p>
+                </div>
+                <div class="col-md-4">
+                    <p><strong>Conducted at:</strong> {{ $checklist->conducted_at->toDayDateTimeString() }}</p>
+                </div>
+            </div>
+            @foreach($checklist->cl_sections as $section)
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>{{ $section->cl_section_template->name }} {{ $section->cl_section_template->subsection_titles }}</h2>
+                        @foreach($section->cl_subsections as $subsection)
+                            @if(count($subsection->cl_questions) > 0 || strlen($subsection->comments) > 0)
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h4>{{ $subsection->cl_subsection_template->name }}</h4>
+                                        @if(strlen($subsection->comments) > 0)
+                                            <p><strong>Notes:</strong> {{ $subsection->comments or 'none' }}</p>
+                                        @endif
+                                        @foreach($subsection->cl_questions as $question)
+                                            @if(!$question->pass)
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <p><strong>{{ $i++ }}. {{ $question->cl_question_template->question }} : </strong>{{ $question->answer }}</p>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        @foreach($question->question_images as $image)
+                                                                <img src="{{ Request::root() }}/photos/{{ $image->filename }}" style="max-width:30%; margin-left:10%; margin-right:10%" />
+                                                        @endforeach
+                                                    </div>
+                                                </div>
+                                            @endif
+                                        @endforeach
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @stop
