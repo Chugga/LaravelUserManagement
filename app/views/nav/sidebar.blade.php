b5d76ffd806925344dd324a04962218e4e443775
Tim Clark <tim@agylelabs.com>
2015-02-22 22:24:27 +1100

    Fixed styling on pdfs.

--- app/views/nav/sidebar.blade.php
+++ app/views/nav/sidebar.blade.php
@@ -12,11 +12,11 @@
                 @if($item['privilege'] <= Auth::user()->privilege)
                     @if(isset($item['route_name']))
                         <li class="{{Route::currentRouteName() == $item['route_name'] ? 'active' : ''}}">
-                            <a href="{{URL::route($item['route_name'])}}" style="background-color:#2F4050;"><span class="nav-label"><i class="fa {{ $item['icon'] }}"></i>{{ $item['display'] }}</span></a>
+                            <a href="{{URL::route($item['route_name'])}}" style="background-color:#2F4050;"><i class="fa {{ $item['icon'] }}"></i><span class="nav-label">{{ $item['display'] }}</span></a>
                         </li>
                     @else
                         <li class="{{in_array(Route::currentRouteName(),NavHelper::routes($item['links']))?'active' : ''}} ">
-                            <a href=""><i class="fa {{ $item['icon'] }}"></i> <span class="nav-label">{{ $item['display'] }}</span> <span class="fa arrow"></span></a>
+                            <a href=""><i class="fa {{ $item['icon'] }}"></i><span class="nav-label">{{ $item['display'] }}</span> <span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 @foreach($item['links'] as $link)
                                     @if($link['privilege'] <= Auth::user()->privilege)
