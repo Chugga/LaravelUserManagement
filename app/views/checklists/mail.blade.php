c36d173129327ac0c63f1dbb87b599a66dd95cd4
Tim Clark <tim@agylelabs.com>
2015-04-19 19:20:23 +1000

    Added selecting email addresses to checklists.

--- app/views/checklists/mail.blade.php
+++ app/views/checklists/mail.blade.php
@@ -16,9 +16,11 @@
                 <h1>Select Email Addresses to receive Checklist</h1>
                 {{ Form::open(array('route' => array('checklists.mail', $checklist->id), 'method' => 'post')) }}
                 @foreach($checklist->client->client_email_addresses as $email)
-                    <div class="form-group">
-                        {{ Form::label('email', $email->email) }}
-                        {{ Form::checkbox('emails[]', $email->email, false, array('id' => 'email', 'class' => 'form-control')) }}
+                    <div class="checkbox">
+                        <label for="{{ $email->email }}">
+                            {{ $email->email }}
+                            {{ Form::checkbox('emails[]', $email->email, false, array('id' => $email->email, 'class' => 'form-control')) }}
+                        </label>
                     </div>
                 @endforeach
                 {{ Form::submit('Submit', array('class' => 'btn btn-success')) }}
